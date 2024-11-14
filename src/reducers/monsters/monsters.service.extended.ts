4da6233a99b79f7c3fdb46870e6683c46240976e
crossbrz8 <crossbrz8@hotmail.com>
2024-11-13 22:57:22 -0300

    sa

--- src/reducers/monsters/monsters.service.extended.ts
+++ src/reducers/monsters/monsters.service.extended.ts
@@ -1,16 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { API_URL } from '../../constants/env';
-import { Battle } from '../../models/interfaces/battle.interface';
+import { Battle, Players } from '../../models/interfaces/battle.interface';
 
 export const battleMonsters = createAsyncThunk(
   'monsters/battle',
-  async ({
-    monster1Id,
-    monster2Id,
-  }: {
-    monster1Id: string;
-    monster2Id: string;
-  }) => {
+  async ({ monster1Id, monster2Id }: Players) => {
     const response = await fetch(`${API_URL}/battle`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
